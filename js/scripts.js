9abff8b9fccb1c7087c35fc4e559c17aa6ebf6fd
AlejandroGrijalva <23cg0201@itsncg.edu.mx>
2025-10-16 09:45:30 -0600

    ola

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Toggle sidebar
+const toggleBtn = document.querySelector('.toggleSidebar');
+  const sidebar = document.querySelector('.sidebar');
+
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+  });
+
+//Remove Item
